1cdc8c789b0937c6ec62db066beacf3d4758e057
Lrkakuma33 <Lkeenswijk@roc-dev.com>
2019-09-10 09:35:17 +0200

    Gelijkbenige driehoek gemaakt

---
+++ src/h04/Opdracht4a.java
@@ -0,0 +1,14 @@
+package h04;
+import java.awt.*;
+import java.applet.Applet;
+
+public class Opdracht4a extends Applet {
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawLine(150,  250,  200, 150);
+        g.drawLine(250,  250,  200, 150);
+        g.drawLine(150,  250,  250, 250);
+}}
