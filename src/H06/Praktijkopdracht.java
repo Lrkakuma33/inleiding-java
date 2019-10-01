9b9ecf08dcb0f6b984e6cbd1099d1da18cafd161
Lrkakuma33 <Lkeenswijk@roc-dev.com>
2019-10-01 11:30:07 +0200

    praktijkopdracht van double naar int

--- src/H06/Praktijkopdracht.java
+++ src/H06/Praktijkopdracht.java
@@ -1,6 +1,28 @@
 package H06;
 
-public class Praktijkopdracht {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Praktijkopdracht extends Applet {
+    double getal1;
+    double getal2;
+    double getal3;
+    double uitkomst;
+
+    public void init() {
+        getal1 = 5.9;
+        getal2 = 6.3;
+        getal3 = 6.9;
+        uitkomst = (getal1 + getal2 + getal3) / 3;
+        uitkomst *= 10;
+        uitkomst = (int) uitkomst;
+        uitkomst = uitkomst / 10;
+
+    }
 
 
+    public void paint(Graphics g) {
+        g.drawString("" + uitkomst, 50, 50);
+
+    }
 }
